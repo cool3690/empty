90a64d75eb143df32235f6ad4dc16f3772feeccc
cool3690 <cool3690@yahoo.com.tw>
2021-03-23 16:32:02 +0800

    0317 car place multi ok

---
+++ app/src/main/java/com/example/empty/Carplacemuti2.java
@@ -0,0 +1,105 @@
+package com.example.empty;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class Carplacemuti2 extends AppCompatActivity {
+    WebView myweb;
+    String account="",passwd="",names="",course_num="";
+    Spinner choose;
+    Button back;
+    private Menu menu;
+    String course[]= {"KLE-5592" ,"788-UG","785-UG ","233-VG","787-VG"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carplacemuti2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+
+        choose=(Spinner)findViewById(R.id.choose);
+        back=(Button) findViewById(R.id.back);
+        ArrayAdapter<String> choosespn=new ArrayAdapter<String>(this,android.R.layout.
+                simple_spinner_dropdown_item,course);
+
+        choose.setAdapter(choosespn);
+
+        choose.setOnItemSelectedListener(chbtn);
+        myweb = (WebView) findViewById(R.id.webview);
+
+        myweb.getSettings().setBuiltInZoomControls(true);
+        myweb.getSettings().setJavaScriptEnabled(true);
+        myweb.setWebViewClient(new WebViewClient());
+
+        myweb.loadUrl("http://vehicle.chansing.com.tw/car/map3.php");
+        back.setOnClickListener(backbtn);
+
+    }
+    private Button.OnClickListener backbtn=new Button.OnClickListener(){//軌跡
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.setClass(Carplacemuti2.this, Home.class);
+            startActivity(intent);
+        }
+    };
+    private Spinner.OnItemSelectedListener chbtn= new Spinner.OnItemSelectedListener(){
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View v,
+                                   int position, long id) {
+            int tmp=position+1;
+
+            // dbplace.executeQuery(tmp+"");
+            //  myweb.loadUrl("http://vehicle.chansing.com.tw/car/map"+tmp+".php");
+            myweb.loadUrl("http://vehicle.chansing.com.tw/car/map3.php?num="+position);
+            //  myweb.scrollTo(0,1400);    http://vehicle.chansing.com.tw/car/map1.php?num=www.google.com
+            //  sel=parent.getSelectedItem().toString();
+
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // TODO Auto-generated method stub
+        }
+    };
+    public void onLoadResource(WebView view, String url) {
+
+        myweb.scrollTo(0,1400);
+
+    }
+
+    public void onBackPressed() {
+        if (myweb.canGoBack()) {
+            myweb.goBack();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+
+}
--- app/src/main/java/com/example/empty/Home.java
+++ app/src/main/java/com/example/empty/Home.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-Button stamp,place,track,real,placemuti;
+Button stamp,place,track,real,placemuti,placemuti2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +22,13 @@ Button stamp,place,track,real,placemuti;
         track=(Button)findViewById(R.id.track);
         real=(Button)findViewById(R.id.real);
         placemuti=(Button)findViewById(R.id.placemuti);
+        placemuti2=(Button)findViewById(R.id.placemuti2);
         stamp.setOnClickListener(stampbtn);
         place.setOnClickListener(placebtn);
         track.setOnClickListener(trackbtn);
         real.setOnClickListener(realbtn);
         placemuti.setOnClickListener(placemutibtn);
+        placemuti2.setOnClickListener(placemuti2btn);
         real.setVisibility(View.GONE);
     }
     private Button.OnClickListener stampbtn=new Button.OnClickListener(){//打卡
@@ -66,6 +68,14 @@ Button stamp,place,track,real,placemuti;
         public void onClick(View v) {
             Intent intent=new Intent();
             intent.setClass(Home.this, Carplacemuti.class);
+            startActivity(intent);
+        }
+    };
+    private Button.OnClickListener placemuti2btn=new Button.OnClickListener(){//打卡
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            intent.setClass(Home.this, Carplacemuti2.class);
             startActivity(intent);
         }
     };
